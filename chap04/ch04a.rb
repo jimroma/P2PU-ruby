f19d54ecb6523218cbf5f33b78e460f35863e54b
Jim <jim@jimroma.com>
2011-04-16 23:09:34 -0400

    week1

---
+++ chap04/ch04a.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+puts 'Enter First Name:'
+fname = gets
+puts 'Enter Second Name:'
+sname = gets
+puts 'Enter Last Name:'
+lname = gets
+puts 'Greetings ' + fname.chomp + ' ' + sname.chomp + ' ' + lname.chomp + '.'
---
+++ chap04/ch04b.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+puts 'What\'s your favorite number:'
+num = gets
+fav = num.to_i + 1
+puts 'Really? ' + fav.to_s.chomp + ' is a much better number' 
+
+
+
