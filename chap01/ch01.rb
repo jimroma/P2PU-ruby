be4ded6dc5efebcc875ee7e1bf7513d3cd1339c9
Jim <jim@jimroma.com>
2011-04-16 23:08:31 -0400

    week1

---
+++ chap01/ch01.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+
+puts "Hours in a year:"
+puts 24 * 365
+
+puts "Minutes in a decade:"
+puts 60*24*365*10
+
+#puts "Seconds in a year:"
+#puts (24 * 365 * 360)
+
+puts "How many seconds old are you"
+puts (24 * 365 * 360 * 39)
+
+puts "1031 million seconds is how many years"
+puts 103100000.00/(24 * 365 * 360)
+
